28d8d181b6f66211045a6a5b61cc08ea77de25bc
Omolade Hassan <omoladehassan996@gmail.com>
2022-10-19 21:14:13 +0100

    added Loading

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const randomComment = document.querySelector(".random-comment")
-    const btn = document.querySelector(".btn")
-    const apiUrl = "https://jsonplaceholder.typicode.com/comments/";
+  const randomComment = document.querySelector(".random-comment");
+  const btn = document.querySelector(".btn");
+  const apiUrl = "https://jsonplaceholder.typicode.com/comments/";
 
-    async function getJoke() {
-      const randomNum = Math.floor(Math.random() * 500) + 1;
-      const response = await fetch(apiUrl + randomNum);
-      const data = await response.json();
+  async function getJoke() {
+    btn.innerText = "Loading...";
+    const randomNum = Math.floor(Math.random() * 500) + 1;
+    const response = await fetch(apiUrl + randomNum);
+    const data = await response.json();
+    btn.innerText = "GET COMMENT";
 
-      randomComment.innerHTML = `Comments: ${data.body}`
-    }
+    randomComment.innerHTML = `Comments: ${data.body}`;
+  }
 
-    btn.addEventListener("click", getJoke);
-})
+  btn.addEventListener("click", getJoke);
+});
